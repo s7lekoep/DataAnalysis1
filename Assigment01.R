e2d8ffde5484be1e19b562ae6e690134cbc4e5ea
s7lekoep <koepchen-Thomae@web.de>
2021-04-21 20:58:31 +0200

    Seminar02

--- Assigment01.R
+++ Assigment01.R
@@ -1,5 +1,6 @@
 #install.packages("tidyverse")
 #library(tidyverse)
+#first some play around with the participants_data
 urlfile = "https://raw.githubusercontent.com/CWWhitney/teaching_R/master/participants_data.csv"
 participants_data <- read.csv(url(urlfile))
 head(participants_data)
@@ -21,26 +22,34 @@ participants_data %>%
 #dplyr####
 #install.packages("dplyr")
 #install.packages("ggplot2")
-library(dplyr)
-library(ggplot2)
+#library(dplyr)
+#library(ggplot2)
+#elect: carat and price
 diamonds$carat
 diamonds$price
-dia<-diamonds
-filter(diamonds,carat >0.5 )
-filter(diamonds,carat >0.5& carat<0.9 )
 
+#filter: only where carat is > 0.5
+filter(diamonds,carat >0.5 )
+#rename: rename price as cost
+dia<-diamonds #crate new varable to change colum name
 dia<-rename(diamonds, cost=price)#erst der neue Name dann der alte
+#mutate: create a variable with ‘expensive’ if greater than mean of cost and ‘cheap’ otherwise
 
 dia<-mutate(dia,#neue Spalte mit dem Namen "name" und wenn Preis des Diamanten>mean ist dann expensive sonst cheap
-       name=ifelse(cost>mean(cost),#aber keine echte spalte kann nicht drauf zugreifen 
+       status=ifelse(cost>mean(cost),#aber keine echte spalte kann nicht drauf zugreifen 
         "expensive","cheap"))
+#group_by: split into cheap and expensive
+dia%>%
+  group_by(status)
+
+#summarize: give some summary statistics of your choice
 
 summarize(dia,
           mean(cost),
           mean(carat))
 
 dia %>% 
-  group_by(name) %>% #
+  group_by(status) %>% #mean & median of the group cheap and expensive 
   summarize(mean(cost), 
             median(carat))
 
